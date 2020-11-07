1fac79476410999af57cc468603cc28a6b7cbb56
scu326 <memmi.hichem@tieto.com>
2020-11-07 23:22:41 +0100

    labb

--- src/main/scala/generics/test.scala
+++ src/main/scala/generics/test.scala
@@ -43,17 +43,24 @@ object main {
   def findMeRec(list: List[Human]): Unit = {
     list match {
       case Nil => Nil
-      case head :: tail => head match {
-        case Human("hichem", _, _, _) => {
-          findMeRec(tail)
-          println("it's me")
+      case head :: tail =>
+        head match {
+          case Human("hichem", _, _, _) => {
+            findMeRec(tail)
+            println("it's me")
+          }
+          case _ =>
+            findMeRec(tail)
+            println("drop out")
         }
-        case _ =>
-          findMeRec(tail)
-          println("drop out")
-      }
     }
+  }
 
+  def findMeUsedByFold(list: List[Human], h: Human): List[Human] = {
+    h match {
+      case Human("hichem", _, _, _) => h :: list
+      case _                        => list
+    }
   }
 
   def main(args: Array[String]) = {
@@ -61,26 +68,31 @@ object main {
     val h2 = Human("ala", "ati", 28, 100000);
     val h3 = Human("hichem", "hmemmi", 27, 99999999);
 
-    val list: List[Human] = List(h1, h2, h3)
+    val list: List[Human] = List(h1, h2, h3, h1, h1, h1, h1)
     println("iterative")
-    val t4 = System.nanoTime()
+    val t0 = System.nanoTime()
     findMe(list)
-    val t5 = System.nanoTime()
-
-    println("Elapsed time iterative: " + (t5 - t4) + "ns")
+    val t1 = System.nanoTime()
+    println("Elapsed time iterative: " + (t1 - t0) + "ns")
 
     println("foreach")
-    val t0 = System.nanoTime()
-    findMeForEach(list)
-    val t1 = System.nanoTime()
-
-    println("Elapsed time foreach: " + (t1 - t0) + "ns")
-    println("recursive")
     val t2 = System.nanoTime()
-    findMeRec(list)
+    findMeForEach(list)
     val t3 = System.nanoTime()
+    println("Elapsed time foreach: " + (t3 - t2) + "ns")
 
-    println("Elapsed time recursive: " + (t3 - t2) + "ns")
+    println("recursive")
+    val t4 = System.nanoTime()
+    findMeRec(list)
+    val t5 = System.nanoTime()
+    println("Elapsed time recursive: " + (t5 - t4) + "ns")
+
+    println("foldLeft")
+    val t6 = System.nanoTime()
+    val f = list.foldLeft(List.empty[Human]) { findMeUsedByFold }
+    val t7 = System.nanoTime()
+    println(f)
+    println("Elapsed time foldLeft: " + (t7 - t6) + "ns")
 
     h1.eat
     println(h1 > h2);
